8e8663fd69aa2ee74049e362ed71260aa0fb170a
unknown <leeviva@gmail.com>
2022-01-18 19:01:53 +0200

    ekaa palautusta varten

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Tämän tekstin tarkoitus on vain havainnollistaa, että muutoksia on tehty muuten tyhjään projektiin.</Text>
       <StatusBar style="auto" />
     </View>
   );
